3785bc99a89af2ed4286983de32b4c4129091c88
Edmond Soun <edmond.soun@gmail.com>
2022-07-28 11:41:13 -0700

    code updates through step 2

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+RESPONSES = []
+
+@app.get("/")
+def start_survey():
+    
+    title = survey.title 
+    instructions = survey.instructions
+
+    return render_template("survey_start.html",title=title, instructions=instructions)
