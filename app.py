49d2d25e1bc3236367b127fd9d7f4c14439869d3
Emily Chen <sticky_sugar@hotmail.com>
2022-07-28 12:29:07 -0700

    create redirect page to first question, update question html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -12,8 +12,24 @@ RESPONSES = []
 
 @app.get("/")
 def start_survey():
-    
-    title = survey.title 
+
+    title = survey.title
     instructions = survey.instructions
 
     return render_template("survey_start.html",title=title, instructions=instructions)
+
+@app.post("/begin")
+def redirect_page():
+    return redirect("/questions/0")
+
+@app.get("/questions/<int:question_number>")
+def questions(question_number):
+
+    question = survey.questions[question_number]
+    choices = question.choices
+
+    return render_template("question.html", question = question, choices = choices)
+
+@app.post("/answer")
+def store_answer():
+    request.form["answer"]
